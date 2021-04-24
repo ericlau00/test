423874013ee7b96d3e54024a6ca114f5eb5c6978
ericlau00 <ericlau41802@gmail.com>
2021-04-24 16:07:09 -0400

    Add tooltip

--- main.js
+++ main.js
@@ -38,7 +38,46 @@ window.onload = async () => {
     let circle = 
         leaf.append('circle')
             .attr('r', d => d.r)
+            .attr('stroke', 'black')
+            .attr('stroke-width', 0)
             .attr('fill', d => 'color' in d.data ? d.data.color : 'gray')
+
+    let tooltip = 
+        d3.select('body')
+            .append('div')
+                .attr('class', 'd3-tip')
+                .style('position', 'absolute')
+                .style('visibility', 'hidden')
+
+    circle
+        .on('mouseenter', function(e, d) {
+            let domain = d.data.domain;
+            let label = ("label" in d.data) 
+                ? d.data.label 
+                : domain.charAt(0).toUpperCase() + domain.slice(1)
+
+            tooltip
+                .style('visibility', 'visible')
+                .html(`
+                    <b>${label}</b> (${domain})<br>
+                    ${d.data.visits} visits`)
+            d3.select(this)
+                .attr('stroke-width', 1)
+        })
+        .on('mousemove', function(e) {
+            let x = e.pageX + 5;
+            let shift = (x > width * 0.8) ? x - 170 : x;
+            tooltip
+                .style('visibility', 'visible')
+                .style('top', `${e.pageY - 30}px`)
+                .style('left', `${shift}px`)
+        })
+        .on('mouseleave', function() {
+            tooltip
+                .style('visibility', 'hidden');
+            d3.select(this)
+                .attr('stroke-width', 0)
+        })
 
     let head = document.getElementById('head-graphic');
     let domain_set = await d3.json('./domain_set.json');
