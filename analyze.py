503fd8cb98ec191c3cd44a9cd705612ca2b525ec
ericlau00 <ericlau41802@gmail.com>
2021-04-24 04:28:44 -0400

    Add flat bubble chart'

--- analyze.py
+++ analyze.py
@@ -32,7 +32,7 @@ for row in cur.execute(
     """
         SELECT datetime(v.visit_time + 978307200, 'unixepoch', 'localtime') AS date, v.visit_time, v.title, i.domain_expansion, i.url, i.visit_count
         FROM history_items i LEFT JOIN history_visits v ON i.id = v.history_item
-        WHERE date BETWEEN '2021-01-18 00:00:00' AND '2021-01-30 00:00:00' 
+        WHERE date >= '2021-01-18 00:00:00'
         ORDER BY v.visit_time ASC;
     """
     ):
@@ -47,20 +47,37 @@ for row in cur.execute(
                 domain = fix_domain(row)
                 count += 1
                 domains[domain] = domains.get(domain, 0) + 1
-                print(row[3])
+                # print(row[3])
                 # domain_set.add(domain)
     else:
         domain = fix_domain(row)
         count += 1
         domains[domain] = domains.get(domain, 0) + 1
-        print(row[3])
+        # print(row[3])
         # domain_set.add(domain)
 
     prev = row
 
-# pprint(sorted(domains.items(), key=lambda x:x[1]))
-# 
-domain_set = [item[0] for item in domains.items() if item[1] > 10]
+other = 0
+remove = list()
+for domain in domains:
+    if domains[domain] < 19:
+        other += domains[domain]
+        remove.append(domain)
 
-with open('domain_set.json', 'w') as f:
-    dump(domain_set, f, indent=2)
+for d in remove:
+    domains.pop(d)
+
+domains['other'] = domains.pop(None) + other
+
+d = [{'domain': item[0], 'visits': item[1]} for item in domains.items()]
+d = sorted(d, key=lambda item: item['visits'], reverse=True)
+# pprint(sorted(domains.items(), key=lambda x:x[1]))
+
+# domain_set = [item[0] for item in domains.items() if item[1] > 10]
+
+# with open('domain_set.json', 'w') as f:
+#     dump(domain_set, f, indent=2)
+
+with open('domain_top.json', 'w') as f:
+    dump(d, f, indent=2)
--- main.js
+++ main.js
@@ -7,9 +7,41 @@ const delay = time => {
 };
 
 window.onload = async () => {    
+    let domain_top = await d3.json('./domain_top.json');
+    console.log(domain_top.length);
+
+    let bubble = 
+        d3.select('#bubble');
+
+    const width = bubble.select(function() { return this.parentNode }).node().clientWidth;
+    const height = width;
+
+    bubble
+        .attr('viewBox', [0, 0, width, height])
+        .attr('font-family', 'sans-serif');
+
+    let hierarchy = 
+        d3.hierarchy({children: domain_top}).sum(d => d.visits);
+
+    let pack = 
+        d3.pack()
+            .size([width, height])
+            .padding(3)
+            (hierarchy);
+
+    let leaf = 
+        bubble.selectAll('g')
+            .data(pack.leaves())
+            .join('g')
+                .attr('transform', d => `translate(${d.x + 1}, ${d.y + 1})`)
+    
+    let circle = 
+        leaf.append('circle')
+            .attr('r', d => d.r)
+            .attr('fill', d => 'color' in d.data ? d.data.color : 'gray')
+
     let head = document.getElementById('head-graphic');
     let domain_set = await d3.json('./domain_set.json');
-
 
     for (const domain of domain_set) {
         for (const letter of domain) {
@@ -17,4 +49,5 @@ window.onload = async () => {
             head.textContent += letter;
         }
     }
+        
 }
